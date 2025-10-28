52c7072a50739d20df355fe40c02a9c13b874734
Suhrob <suhrob_kalandarov@outlook.com>
2025-10-28 16:27:52 +0500

    Added path Constants

---
+++ src/main/java/org/exp/banduapp/util/Constants.java
@@ -0,0 +1,27 @@
+package org.exp.banduapp.util;
+
+public interface Constants {
+    String WAY_ONE = "/*";
+    String WAY_ALL = "/**";
+
+    String API = "/api";
+    String V1 = "/v1";
+
+    String AUTHORIZATION = "Authorization";
+    String TOKEN_PREFIX = "Bearer ";
+
+    String AUTH = "/auth";
+    String LOGIN = "/login";
+    String REGISTER = "/register";
+    String VERIFY = "/verify";
+    String LOGOUT = "/logout";
+    String REFRESH = "/refresh";
+
+    String ADMIN = "/admin";
+    String USER = "/user";
+    String USERS = "/users";
+
+    String ROLE_SUPER_ADMIN ="ROLE_SUPER_ADMIN";
+    String ROLE_ADMIN ="ROLE_ADMIN";
+    String ROLE_USER ="ROLE_USER";
+}

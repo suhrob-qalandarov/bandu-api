7bb1a4b98da6b9a25cf6f6894893256119fc8a2a
Suhrob <suhrob_kalandarov@outlook.com>
2025-10-28 15:36:05 +0500

    Initialize project with Spring Boot setup, Maven configuration, and necessary dependencies

---
+++ src/main/java/org/exp/banduapp/BanduAppApplication.java
@@ -0,0 +1,13 @@
+package org.exp.banduapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BanduAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BanduAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/exp/banduapp/BanduAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.exp.banduapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BanduAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

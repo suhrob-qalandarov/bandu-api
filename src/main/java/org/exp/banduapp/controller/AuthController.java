a8c65bdcc6f0112a61b81bed0dd09a73c51d8812
Suhrob <suhrob_kalandarov@outlook.com>
2025-10-28 21:23:02 +0500

    Expand `AuthController` with user registration and verification endpoints
    - Add `/register` endpoint to handle user registration with OTP generation.
    - Add `/verify` endpoint for OTP validation and user authentication.
    - Inject `AuthService` to support authentication operations.

--- src/main/java/org/exp/banduapp/controller/AuthController.java
+++ src/main/java/org/exp/banduapp/controller/AuthController.java
@@ -1,14 +1,16 @@
 package org.exp.banduapp.controller;
 
+import jakarta.validation.constraints.Size;
 import lombok.RequiredArgsConstructor;
+import org.exp.banduapp.models.dto.request.RegisterReq;
+import org.exp.banduapp.models.dto.response.LoginRes;
 import org.exp.banduapp.models.dto.response.UserRes;
 import org.exp.banduapp.models.entities.User;
+import org.exp.banduapp.service.face.AuthService;
 import org.exp.banduapp.service.face.UserService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import static org.exp.banduapp.util.Constants.*;
 
@@ -18,6 +20,7 @@ import static org.exp.banduapp.util.Constants.*;
 public class AuthController {
 
     private final UserService userService;
+    private final AuthService authService;
 
     @GetMapping("/me")
     public ResponseEntity<UserRes> getUserData(@AuthenticationPrincipal User user) {
@@ -25,4 +28,18 @@ public class AuthController {
         return ResponseEntity.ok(userRes);
     }
 
+    @PostMapping("/register")
+    public ResponseEntity<String> getUserData(@RequestBody RegisterReq registerReq) {
+        String otpCode = authService.saveDataAndSendVerifyCode(registerReq);
+        return ResponseEntity.ok(otpCode);
+    }
+
+    @PostMapping("/verify")
+    public ResponseEntity<LoginRes> getUserData(
+            @RequestParam("phoneNumber") String phoneNumber,
+            @RequestParam("otpCode") @Size(min = 6, max = 6) String otpCode
+    ) {
+        LoginRes loginRes = authService.verifyAndSendUserData(phoneNumber, otpCode);
+        return ResponseEntity.ok(loginRes);
+    }
 }

164a7cfb9624180f45bf83f6e406ae71f02a516c
Suhrob <suhrob_kalandarov@outlook.com>
2025-10-29 12:48:36 +0500

    Add logging and validation to `RoleServiceImpl#getOrCreateRoleByName`
    - Integrated `@Slf4j` for logging in `RoleServiceImpl`.
    - Added validation to prevent null `RoleName` with an `IllegalArgumentException`.
    - Enhanced logging during role creation process.

--- src/main/java/org/exp/banduapp/service/impl/RoleServiceImpl.java
+++ src/main/java/org/exp/banduapp/service/impl/RoleServiceImpl.java
@@ -1,12 +1,14 @@
 package org.exp.banduapp.service.impl;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.exp.banduapp.models.entities.Role;
 import org.exp.banduapp.models.enums.RoleName;
 import org.exp.banduapp.repository.RoleRepository;
 import org.exp.banduapp.service.face.RoleService;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class RoleServiceImpl implements RoleService {
@@ -15,8 +17,16 @@ public class RoleServiceImpl implements RoleService {
 
     @Override
     public Role getOrCreateRoleByName(RoleName roleName) {
+
+        if (roleName == null) {
+            throw new IllegalArgumentException("Role nomi bo'sh bo'lmasligi kerak");
+        }
+
         return roleRepository.findByName(roleName)
-                .orElse(createNewRole(roleName));
+                .orElseGet(() -> {
+                    log.info("Yangi rol yaratilmoqda: {}", roleName);;
+                    return createNewRole(roleName);
+                });
     }
 
     private Role createNewRole(RoleName roleName) {

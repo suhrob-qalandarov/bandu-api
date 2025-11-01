647bdd1a22225608f3c519843250b5291ab1bcfb
Suhrob <suhrob_kalandarov@outlook.com>
2025-11-01 20:06:22 +0500

    Add admin roadmap service methods and improve entity conversion
    - Introduced `getAdminRoadMaps`, `getAdminRoadMapRes`, and `createAndReturnRes` methods in `RoadMapService` and its implementation.
    - Enhanced `convertToRoadMapRes` and added `convertToEntity` for better handling of roadmap entity and task transformations.
    - Improved empty collection handling for roadmap tasks.

--- src/main/java/org/exp/banduapp/service/impl/RoadMapServiceImpl.java
+++ src/main/java/org/exp/banduapp/service/impl/RoadMapServiceImpl.java
@@ -1,14 +1,19 @@
 package org.exp.banduapp.service.impl;
 
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
+import org.exp.banduapp.models.dto.request.admin.RoadMapReq;
+import org.exp.banduapp.models.dto.request.admin.RoadMapTaskReq;
 import org.exp.banduapp.models.dto.response.RoadMapRes;
 import org.exp.banduapp.models.dto.response.RoadMapTaskRes;
 import org.exp.banduapp.models.entities.RoadMap;
+import org.exp.banduapp.models.entities.RoadMapTask;
 import org.exp.banduapp.repository.RoadMapRepository;
 import org.exp.banduapp.service.face.RoadMapService;
 import org.exp.banduapp.service.face.RoadMapTaskService;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -25,16 +30,61 @@ public class RoadMapServiceImpl implements RoadMapService {
                 .toList();
     }
 
-    private RoadMapRes convertToRoadMapRes(RoadMap roadMap){
-        List<RoadMapTaskRes> taskResList = roadMap.getTasks().stream()
-                .map(roadMapTaskService::convertToRes)
+    @Override
+    public List<RoadMapRes> getAdminRoadMaps() {
+        return roadMapRepository.findAll().stream()
+                .map(this::convertToRoadMapRes)
                 .toList();
+    }
+
+    @Override
+    public RoadMapRes getAdminRoadMapRes(Long roadmapId) {
+        RoadMap roadMap = roadMapRepository.findById(roadmapId)
+                .orElseThrow(() -> new EntityNotFoundException("Roadmap not found with id: " + roadmapId));
+        return convertToRoadMapRes(roadMap);
+    }
+
+    @Override
+    public RoadMapRes createAndReturnRes(RoadMapReq roadMapReq) {
+        RoadMap convertedRoadMap = convertToEntity(roadMapReq);
+        RoadMap savedRoadMap = roadMapRepository.save(convertedRoadMap);
+        return convertToRoadMapRes(savedRoadMap);
+    }
+
+    private RoadMapRes convertToRoadMapRes(RoadMap roadMap){
+        List<RoadMapTaskRes> taskResList = new ArrayList<>();
+
+        if (!roadMap.getTasks().isEmpty()) {
+            List<RoadMapTaskRes> roadMapTaskResList = roadMap.getTasks().stream()
+                    .map(roadMapTaskService::convertToRes)
+                    .toList();
+            taskResList.addAll(roadMapTaskResList);
+        }
+
         return RoadMapRes.builder()
                 .id(roadMap.getId())
                 .title(roadMap.getTitle())
                 .description(roadMap.getDescription())
                 .result(roadMap.getResult())
                 .tasks(taskResList)
+                .build();
+    }
+
+    private RoadMap convertToEntity(RoadMapReq roadMapReq) {
+        List<RoadMapTaskReq> roadMapReqList = roadMapReq.roadMapTasksRequestList();
+        List<RoadMapTask> roadMapTasks = new ArrayList<>();
+
+        if (!roadMapReqList.isEmpty()) {
+            List<RoadMapTask> convertedRoadMapTaskList = roadMapTaskService.convertListToEntityList(roadMapReqList);
+            roadMapTasks.addAll(convertedRoadMapTaskList);
+        }
+
+        return RoadMap.builder()
+                .title(roadMapReq.title())
+                .description(roadMapReq.description())
+                .result(roadMapReq.result())
+                .tasks(roadMapTasks)
+                .visibility(roadMapReq.visibility())
                 .build();
     }
 }

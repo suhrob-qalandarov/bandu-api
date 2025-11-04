1da1053d2ae84005b52bdf8b63f6f51104e08aab
Suhrob <suhrob_kalandarov@outlook.com>
2025-11-04 19:53:12 +0500

    Enhance `AuthController` with new endpoints and improve Swagger documentation
    - Added `/reset-password` endpoint for handling password reset via OTP.
    - Introduced `/change-phone-number` endpoint for updating registered phone number after OTP verification.
    - Refined Swagger annotations for better endpoint descriptions and responses.

--- src/main/java/org/exp/banduapp/controller/AuthController.java
+++ src/main/java/org/exp/banduapp/controller/AuthController.java
@@ -7,9 +7,6 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Pattern;
-import jakarta.validation.constraints.Size;
 import lombok.RequiredArgsConstructor;
 import org.exp.banduapp.models.dto.request.LoginReq;
 import org.exp.banduapp.models.dto.request.RegisterReq;
@@ -36,19 +33,34 @@ public class AuthController {
     private final UserService userService;
     private final AuthService authService;
 
-    @Operation(summary = "Current user", description = "Client data from token")
-    @ApiResponse(responseCode = "200", description = "Client information")
+    @Operation(
+            summary = "Current user",
+            description = "Client data from token"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "Client information"
+    )
     @GetMapping("/me")
     public ResponseEntity<UserRes> getClientData(@AuthenticationPrincipal User user) {
         UserRes userRes = userService.convertToUserResponse(user);
         return ResponseEntity.ok(userRes);
     }
 
-    @Operation(summary = "Sign in", description = "Login via phone and password")
+    @Operation(
+            summary = "Sign in",
+            description = "Login via phone and password"
+    )
     @ApiResponses({
-            @ApiResponse(responseCode = "200", description = "Successful login",
-                    content = @Content(schema = @Schema(implementation = LoginRes.class))),
-            @ApiResponse(responseCode = "400", description = "Incorrect phone or password")
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Successful login",
+                    content = @Content(schema = @Schema(implementation = LoginRes.class))
+            ),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "Incorrect phone or password"
+            )
     })
     @PostMapping("/login")
     public ResponseEntity<LoginRes> loginUser(@RequestBody LoginReq loginReq) {
@@ -56,19 +68,59 @@ public class AuthController {
         return ResponseEntity.ok(loginRes);
     }
 
-    @Operation(summary = "Sign up", description = "Create a new user → OTP will be sent")
-    @ApiResponse(responseCode = "200", description = "OTP code will be returned (for example: 123456)")
+    @Operation(
+            summary = "Sign up",
+            description = "Create a new user → OTP will be sent"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "OTP code will be returned (for example: 123456)"
+    )
     @PostMapping("/register")
     public ResponseEntity<String> registerClient(@Valid @RequestBody RegisterReq registerReq) {
         String otpCode = authService.registerClientAndSendVerifyCode(registerReq);
         return ResponseEntity.ok(otpCode);
     }
 
-    @Operation(summary = "Confirm with OTP code", description = "Account activation via OTP")
-    @ApiResponse(responseCode = "200", description = "Account activated - (UserData + JWT token)")
+    @Operation(
+            summary = "Confirm with OTP code",
+            description = "Account activation via OTP"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "Account activated - (UserData + JWT token)"
+    )
     @PostMapping("/verify")
     public ResponseEntity<LoginRes> verifyClient(@RequestBody VerificationReq verificationReq) {
         LoginRes loginRes = authService.verifyClient(verificationReq.phoneNumber(), verificationReq.otpCode());
         return ResponseEntity.ok(loginRes);
     }
+
+    @Operation(
+            summary = "Reset password via OTP code",
+            description = "Allows the user to reset their password using a verified OTP code."
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "Password successfully reset."
+    )
+    @PostMapping("/reset-password")
+    public ResponseEntity<?> resetClientPassword() {
+        return ResponseEntity.ok().build();
+    }
+
+
+    @Operation(
+            summary = "Change registered phone number",
+            description = "Allows the user to change their registered phone number after OTP verification."
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "Phone number successfully changed."
+    )
+    @PostMapping("/change-phone-number")
+    public ResponseEntity<?> changeClientPhoneNumber() {
+        return ResponseEntity.ok().build();
+    }
+
 }
